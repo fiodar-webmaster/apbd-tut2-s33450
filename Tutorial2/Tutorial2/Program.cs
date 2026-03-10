d562528984b6a1e58c8b78c3a5d2b9c1163d45c4
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-10 13:06:25 +0100

    Factorial method implemented

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+static long Factorial(int n)
+{
+    if (n < 0)
+        throw new ArgumentException("Factorial is not defined for negative numbers.");
+    long result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
