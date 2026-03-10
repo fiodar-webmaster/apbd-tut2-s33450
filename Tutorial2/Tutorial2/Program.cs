da452793139925ad64663a9c6397498923d5e5b2
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-10 13:42:10 +0100

    CalculateMin method simplified and refactored

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -76,16 +76,5 @@ static int CalculateMin(int[] values)
 {
     if (values == null || values.Length == 0)
         throw new ArgumentException("Array must not be null or empty.");
-
-    int min = values[0];
-
-    for (int i = 1; i < values.Length; i++)
-    {
-        if (values[i] < min)
-        {
-            min = values[i];
-        }
-    }
-
-    return min;
+    return values.Min();
 }
