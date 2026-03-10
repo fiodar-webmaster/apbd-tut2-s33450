37ebf753b8a520791128ce4d95e87a81bf18813f
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-10 13:11:20 +0100

    IsPrime method - optimized for better performance

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -19,7 +19,7 @@ static bool IsPrime(int n)
 {
     if (n <= 1)
         return false;
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; i <= Math.Sqrt(n); i++)
     {
         if (n % i == 0)
             return false;
