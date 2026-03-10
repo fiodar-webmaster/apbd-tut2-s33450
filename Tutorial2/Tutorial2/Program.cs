693b64c358a04ad834543ba0019e5d0b75aac09d
fiodar-webmaster <murasakibara314@gmail.com>
2026-03-10 13:19:42 +0100

    CalculateAverage method implemented

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -33,3 +33,17 @@ static string ReverseString(string input)
     Array.Reverse(chars);
     return new string(chars);
 }
+
+
+static double CalculateAverage(int[] values)
+{
+    if (values == null || values.Length == 0)
+        throw new ArgumentException("Array must not be null or empty.");
+    int sum = 0;
+    
+    foreach (int value in values)
+    {
+        sum += value;
+    }
+    return (double)sum / values.Length;
+}
