e660a6eef80b848964bdf56c27103b1ff7bf3e78
Fiodar Klimovich <murasakibara314@gmail.com>
2026-03-16 18:53:41 +0100

    CalculateMax implemented

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -34,3 +34,22 @@ static double CalculateAverage(int[] values)
     }
     return sum/values.Length;
 }
+
+static int CalculateMax(int[] values)
+{
+    if (values == null || values.Length == 0)
+    {
+        throw new ArgumentException("Array cannot be null or empty");
+    }
+
+    int max = values[0];
+    foreach (int value in values)
+    {
+        if (value > max)
+        {
+            max = value;
+        }
+    }
+
+    return max;
+}
