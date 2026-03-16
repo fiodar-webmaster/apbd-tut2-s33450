3d225fd7ea483012f5c694b4a81939b7c48e5c5c
Fiodar Klimovich <murasakibara314@gmail.com>
2026-03-16 18:26:14 +0100

    CalculateCircleArea method implemented

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -4,3 +4,9 @@ static bool IsAdult(int age)
 {
     return age >= 18;
 }
+
+static double CalculateCircleArea(double radius)
+{
+    if (radius < 0) return 0;
+    return Math.PI * Math.Pow(radius, 2);
+}
