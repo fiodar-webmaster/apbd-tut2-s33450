b419e7efacd2376061c5e6bab1e539cd0cd07bcc
Fiodar Klimovich <murasakibara314@gmail.com>
2026-03-16 18:27:12 +0100

    CelsiusToFarenheit method implemented

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -10,3 +10,8 @@ static double CalculateCircleArea(double radius)
     if (radius < 0) return 0;
     return Math.PI * Math.Pow(radius, 2);
 }
+
+static double CelsiusToFahrenheit(double celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
